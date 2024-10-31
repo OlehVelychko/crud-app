c2c35f45f3eaae7f4255619b7ac8685f44747054
OlehVelychko <70134299+OlehVelychko@users.noreply.github.com>
2024-10-31 22:43:30 +0200

    Add Person model, DAO, and controller classes for CRUD operations
    - Implement `Person` model to represent individual person data.
    - Add `PersonDAO` class to manage in-memory storage and retrieval of people data.
    - Create `PeopleController` for handling requests related to the `Person` entity:
      - `index` method to display all persons.
      - `show` method to display details of a specific person by ID.
    - Fix attribute handling in `show` method by using `addAttribute` to correctly pass data to views.

---
+++ src/main/java/ua/velychko/springcourse/dao/PersonDAO.java
@@ -0,0 +1,30 @@
+package ua.velychko.springcourse.dao;
+
+import org.springframework.stereotype.Component;
+import ua.velychko.springcourse.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private final List<Person> people;
+    private static int PEOPLE_COUNT;
+
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(++PEOPLE_COUNT, "Alice"));
+        people.add(new Person(++PEOPLE_COUNT, "Oleh"));
+        people.add(new Person(++PEOPLE_COUNT, "Rita"));
+        people.add(new Person(++PEOPLE_COUNT, "Eileen"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+}
